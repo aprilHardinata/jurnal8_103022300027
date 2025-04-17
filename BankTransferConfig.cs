e1c8f124a300f2001454eac712b7803246a4c5d9
Thoriq111 <thoriqmarcello2146@gmail.com>
2025-04-17 11:16:16 +0700

    plikasi bank transfer dengan runtime configuration

---
+++ BankTransferConfig.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace jurnal8_103022300027
+{
+    public class BankTransferConfig
+    {
+
+        public string lang { get; set; }
+        public string name { get; set; }
+        public Transfer transfer {  get; set; }
+        public List <String> methods { get; set; }
+        public Confirmation confirmation { get; set; }
+
+        public class Transfer
+        {
+            public string threshold { get; set; }
+            public string low_fee { get; set; }
+            public string high_fee { get; set; }
+        }
+
+        public class Confirmation
+        {
+            public string en { get; set; }
+            public string id { get; set; }
+        } 
+    }
+
+    public class UIConfig
+    {
+        public BankTransferConfig config;
+        public const String filePath = @"bank_transfer_config.json";
+        public UIConfig() {
+            try
+            {
+                ReadConfigFile();
+            }
+            catch (Exception)
+            {
+                SetDefault();
+                WriteNewConfigFile();
+            }
+        }
+        public BankTransferConfig ReadConfigFile()
+        {
+            String configJsonData = File.ReadAllText(filePath);
+            config = JsonSerializer.Deserialize<BankTransferConfig>(configJsonData);
+            return config;
+        }
+        private void SetDefault() {
+            config = new BankTransferConfig();
+            config.lang = "en";
+            config.transfer.threshold = "25000000";
+            config.transfer.low_fee = "6500";
+            config.transfer.high_fee = " 15000";
+            config.methods = new List<string> { "RTO(real - time)", "SKN", "RTGS", "BI FAST"};
+            config.confirmation.en = "YES";
+            config.confirmation.id = "IYA";
+        }
+        private void WriteNewConfigFile() {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            String jsonString = JsonSerializer.Serialize(config, options);
+            File.WriteAllText(filePath, jsonString);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Linq.Expressions;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using jurnal8_103022300027;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BankTransferConfig config;
+
+        if (File.Exists("bank_transfer_config.json"))
+        {
+            string json = File.ReadAllText("bank_transfer_config.json");
+            config = JsonSerializer.Deserialize<BankTransferConfig>(json);
+        }
+        else
+        {
+            config = new BankTransferConfig(); // default value
+        }
+
+        if (config.lang == "en") {
+            Console.WriteLine("insert uang");
+        }
+    }
+}
+
+
